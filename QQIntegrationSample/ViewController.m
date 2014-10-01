18023d418f4170900eaef6e7ce20b27572653ed0
Yongping Chen <yongping.chen@kii.com>
2014-10-01 16:14:30 +0900

    get access token by QQ SDK

--- QQIntegrationSample/ViewController.m
+++ QQIntegrationSample/ViewController.m
@@ -7,9 +7,15 @@
 //
 
 #import "ViewController.h"
+#import <TencentOpenAPI/TencentOAuth.h>
 
-@interface ViewController ()
+@interface ViewController ()<TencentSessionDelegate>
+{
+    TencentOAuth *tencentOAuth;
+}
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *loginOutButton;
 
+- (IBAction)tapBarButton:(id)sender;
 @end
 
 @implementation ViewController
@@ -23,5 +29,49 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)tapBarButton:(id)sender{
+    
+    UIBarButtonItem *barButton = (UIBarButtonItem *)sender;
+    
+    if ([barButton.title isEqualToString:@"Login"]) {
+        tencentOAuth = [[TencentOAuth alloc] initWithAppId:@"1103266634" andDelegate:self];
+        [tencentOAuth authorize:@[@"get_user_info"] inSafari:NO];
+    }else{
+        [tencentOAuth logout:self];
+    }
+
+    
+}
+
+-(void)tencentDidLogin{
+    if (tencentOAuth.accessToken && 0 != [tencentOAuth.accessToken length])
+    {
+        //  记录登录用户的OpenID、Token以及过期时间
+        NSLog(tencentOAuth.accessToken);
+        _loginOutButton.title = @"Logout";
+        
+    }
+    else
+    {
+        NSLog(@"login failed, no access token returned");
+    }
+}
+
+-(void)tencentDidNotLogin:(BOOL)cancelled
+{
+    NSLog(@"did not login");
+}
+
+-(void)tencentDidNotNetWork
+{
+    NSLog(@"no network available");
+}
+
+-(void)tencentDidLogout{
+    NSLog(@"Did logout");
+    _loginOutButton.title = @"Login";
+}
+
 
 @end
